443fbc3cf13ed8b4484b3004004f5f6a4dfba865
beechnut <cloydster@gmail.com>
2013-07-11 11:45:42 -0400

    Checks validity of API key, downcases dataset on_save

--- lib/census_api/client.rb
+++ lib/census_api/client.rb
@@ -8,18 +8,27 @@ module CensusApi
     def initialize(options = {})
       raise ArgumentError, "You must set an :api_key." unless options.keys.include? :api_key
 
-      # request = self.find('P0010001', 'STATE:02')
-      # raise "Invalid API key." if request[:code] = 302
+      # Use RestClient directly to determine the validity of the API Key
+      path = "http://api.census.gov/data/2010/sf1?key=#{options[:api_key]}&get=P0010001&for=state:01"
+      response = RestClient.get(path)
 
+      if response.body.include? "Invalid Key"
+        raise "'#{options[:api_key]}' is not a valid API key. Check your key for errors, or request a new one at census.gov."
+      end
+      
       @api_key = options[:api_key]
-
       if options[:dataset]
         @dataset = options[:dataset].downcase if DATASETS.include? options[:dataset].downcase
       end
     end
 
     def find(field, level, *within)
+      raise "Client has not been assigned a dataset to query. Try @client.dataset = 'SF1' or anything from #{DATASETS}" if self.dataset.nil?
       Request.find(@dataset, {key: @api_key, fields: field, level: level, within: within})
+    end
+
+    def dataset=(dataset)
+      @dataset = dataset.downcase
     end
 
   end
