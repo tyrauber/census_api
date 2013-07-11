41a01c1c85ae062ba0d6ed1ed4d1b5cc05c80926
beechnut <cloydster@gmail.com>
2013-07-11 11:58:12 -0400

    Changed spec back but VCR is failing because of Client pre-initialization API key check

--- spec/census_api/client_spec.rb
+++ spec/census_api/client_spec.rb
@@ -7,27 +7,27 @@ describe CensusApi::Client do
   end
 
   it 'should initialize with an api_key' do
-    @client = CensusApi::Client.new(api_key: api_key)
+    @client = CensusApi::Client.new(api_key)
     @client.api_key.should == api_key
   end
 
   it 'should initialize with an api_key and dataset' do
     dataset = 'SF1'
-    @client = CensusApi::Client.new(api_key: api_key, dataset: dataset)
+    @client = CensusApi::Client.new(api_key, dataset: dataset)
     @client.api_key.should == api_key
     @client.dataset.should == dataset.downcase
   end
 
   it 'should request sf1' do
     source, options = 'sf1', {:key=> api_key, :fields => 'P0010001', :level => 'STATE:06', :within=>[]}
-    @client = CensusApi::Client.new(api_key: api_key, dataset: source)
+    @client = CensusApi::Client.new(api_key, dataset: source)
     CensusApi::Request.should_receive(:find).with(@client.dataset, options)
     @client.find(options[:fields], options[:level])
   end
 
   it 'should request acs5' do
     source, options = 'acs5', {:key=> api_key, :fields => 'B00001_001E', :level => 'STATE:06', :within=>[]}
-    @client = CensusApi::Client.new(api_key: api_key, dataset: source)
+    @client = CensusApi::Client.new(api_key, dataset: source)
     CensusApi::Request.should_receive(:find).with(@client.dataset, options)
     @client.find(options[:fields], options[:level])
   end
