eba4aa8ddb5081c7aa29d933d413108f1fa6f4fe
beechnut <cloydster@gmail.com>
2013-07-12 12:54:10 -0400

    allow passing in fields as array

--- lib/census_api/client.rb
+++ lib/census_api/client.rb
@@ -22,9 +22,9 @@ module CensusApi
       end
     end
 
-    def find(field, level, *within)
+    def find(fields, level, *within)
       raise "Client has not been assigned a dataset to query. Try @client.dataset = 'SF1' or anything from #{DATASETS}" if self.dataset.nil?
-      Request.find(@dataset, {key: @api_key, fields: field, level: level, within: within})
+      Request.find(@dataset, {key: @api_key, fields: fields, level: level, within: within})
     end
 
     def dataset=(dataset)
--- lib/census_api/request.rb
+++ lib/census_api/request.rb
@@ -12,22 +12,25 @@ module CensusApi
     
     CENSUS_URL = "http://api.census.gov/data/2010"
 
+    def initialize(url, source, options)
+      path = "#{url}/#{source}?#{options.to_params}"
+      @response = RestClient.get(path) do |response, request, result, &block|
+        response
+      end
+      return @response
+    end
+
+
     def self.find(source, options = {})
-      fields = options[:fields].split(",").push("NAME").join(",")
+      fields = options[:fields]
+      fields = fields.split(",").push("NAME").join(",") if fields.kind_of? String
+      fields = fields.push("NAME").join(",") if fields.kind_of? Array
       params = { :key => options[:key], :get => fields, :for => format(options[:level],false) }
       params.merge!({ :in => format(options[:within][0],true) }) if !options[:within].empty?
       request = new(CENSUS_URL, source, params)
       request.parse_response
     end
 
-    def initialize(url, source, options)
-      path = "#{url}/#{source}?#{options.to_params}"
-      @response = RestClient.get(path){ 
-        |response, request, result, &block|
-          response
-      }
-      return @response
-    end
     
     def parse_response
       case @response.code
@@ -42,30 +45,30 @@ module CensusApi
     
     protected
   
-    def self.format(str,truncate)
-      result = str.split("+").map{|s|
-        if s.match(":")
-          s = s.split(":")
-        else 
-          s = [s,"*"]
-        end
-        shp = shapes[s[0].upcase]
-        s.shift && s.unshift(shp['name'].downcase.gsub(" ", "+")) if !shp.nil?
-        s.unshift(s.shift.split("/")[0]) if !s[0].scan("home+land").empty? && truncate
-        s.join(":")
-      }
-      return result.join("+")
-    end 
-    
-    def self.shapes
-      return  @@census_shapes if defined?( @@census_shapes)
-      @@census_shapes = {} 
-      YAML.load_file(File.dirname(__FILE__).to_s + '/../yml/census_shapes.yml').each{|k,v| @@census_shapes[k] = v}
-      return @@census_shapes
+      def self.format(str,truncate)
+        result = str.split("+").map{|s|
+          if s.match(":")
+            s = s.split(":")
+          else 
+            s = [s,"*"]
+          end
+          shp = shapes[s[0].upcase]
+          s.shift && s.unshift(shp['name'].downcase.gsub(" ", "+")) if !shp.nil?
+          s.unshift(s.shift.split("/")[0]) if !s[0].scan("home+land").empty? && truncate
+          s.join(":")
+        }
+        return result.join("+")
+      end 
+      
+      def self.shapes
+        return  @@census_shapes if defined?( @@census_shapes)
+        @@census_shapes = {} 
+        YAML.load_file(File.dirname(__FILE__).to_s + '/../yml/census_shapes.yml').each{|k,v| @@census_shapes[k] = v}
+        return @@census_shapes
+      end
+      
     end
-    
   end
-end
 
 class Hash
    def to_params
