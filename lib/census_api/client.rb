048013773be2cb342ca69e5e3a6440993d9fe8f1
Ty Rauber <tyrauber@mac.com>
2013-07-14 09:21:06 -0700

    Fix specs to use vcd on client initialization. Fix dataset typo in client. Version bump.

--- lib/census_api/client.rb
+++ lib/census_api/client.rb
@@ -3,7 +3,7 @@ module CensusApi
     attr_reader   :api_key, :options
     attr_accessor :dataset
 
-    DATASETS = %w( sf1 ac5 ) # can add more datasets as support becomes available
+    DATASETS = %w( sf1 acs5 ) # can add more datasets as support becomes available
 
     def initialize(api_key, options = {})
       raise ArgumentError, "You must set an api_key." unless api_key
@@ -15,7 +15,7 @@ module CensusApi
       if response.body.include? "Invalid Key"
         raise "'#{api_key}' is not a valid API key. Check your key for errors, or request a new one at census.gov."
       end
-      
+
       @api_key = api_key
       if options[:dataset]
         @dataset = options[:dataset].downcase if DATASETS.include? options[:dataset].downcase
@@ -24,12 +24,7 @@ module CensusApi
 
     def find(fields, level, *within)
       raise "Client has not been assigned a dataset to query. Try @client.dataset = 'SF1' or anything from #{DATASETS}" if self.dataset.nil?
-      Request.find(@dataset, {key: @api_key, fields: fields, level: level, within: within})
+      Request.find(dataset, {key: @api_key, fields: fields, level: level, within: within})
     end
-
-    def dataset=(dataset)
-      @dataset = dataset.downcase
-    end
-
   end
 end
--- spec/census_api/client_spec.rb
+++ spec/census_api/client_spec.rb
@@ -2,34 +2,43 @@ require 'spec_helper'
 
 describe CensusApi::Client do
 
-  it 'should not initialize without an api_key' do
-    lambda { CensusApi::Client.new }.should raise_error
+  describe "client initialization" do
+
+    use_vcr_cassette "initialize_client"
+
+    it 'should not initialize without an api_key' do
+      lambda { CensusApi::Client.new }.should raise_error
+    end
+
+    it 'should initialize with an api_key' do
+      @client = CensusApi::Client.new(api_key)
+      @client.api_key.should == api_key
+    end
   end
 
-  it 'should initialize with an api_key' do
-    @client = CensusApi::Client.new(api_key)
-    @client.api_key.should == api_key
-  end
+  describe "client and dataset initialization" do
 
-  it 'should initialize with an api_key and dataset' do
-    dataset = 'SF1'
-    @client = CensusApi::Client.new(api_key, dataset: dataset)
-    @client.api_key.should == api_key
-    @client.dataset.should == dataset.downcase
-  end
+    use_vcr_cassette "initialize_client_and_dataset"
 
-  it 'should request sf1' do
-    source, options = 'sf1', {:key=> api_key, :fields => 'P0010001', :level => 'STATE:06', :within=>[]}
-    @client = CensusApi::Client.new(api_key, dataset: source)
-    CensusApi::Request.should_receive(:find).with(@client.dataset, options)
-    @client.find(options[:fields], options[:level])
-  end
+    it 'should initialize with an api_key and dataset' do
+      dataset = 'SF1'
+      @client = CensusApi::Client.new(api_key, dataset: dataset)
+      @client.api_key.should == api_key
+      @client.dataset.should == dataset.downcase
+    end
 
-  it 'should request acs5' do
-    source, options = 'acs5', {:key=> api_key, :fields => 'B00001_001E', :level => 'STATE:06', :within=>[]}
-    @client = CensusApi::Client.new(api_key, dataset: source)
-    CensusApi::Request.should_receive(:find).with(@client.dataset, options)
-    @client.find(options[:fields], options[:level])
+    it 'should request sf1' do
+      source, options = 'sf1', {:key=> api_key, :fields => 'P0010001', :level => 'STATE:06', :within=>[]}
+      @client = CensusApi::Client.new(api_key, dataset: source)
+      CensusApi::Request.should_receive(:find).with(@client.dataset, options)
+      @client.find(options[:fields], options[:level])
+    end
+
+    it 'should request acs5' do
+      source, options = 'acs5', {:key=> api_key, :fields => 'B00001_001E', :level => 'STATE:06', :within=>[]}
+      @client = CensusApi::Client.new(api_key, dataset: source)
+      CensusApi::Request.should_receive(:find).with(@client.dataset, options)
+      @client.find(options[:fields], options[:level])
+    end
   end
-  
 end
