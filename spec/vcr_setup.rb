27e7ac252531cd160f1229c3b355d1d30fa5fa7e
Michael Weigle <michael.weigle@gmail.com>
2016-05-11 17:40:43 -0400

    no localhost services, so dont need this

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -4,6 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.configure_rspec_metadata!
   c.hook_into :webmock
-  # c.ignore_localhost = true
   c.default_cassette_options = { record: :new_episodes }
 end
